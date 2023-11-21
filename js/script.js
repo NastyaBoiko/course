64548cdc395e9f6860480ec6b8ba0934dddad9e3
Nastya <nastya@mail.com>
2023-11-21 18:43:12 +0300

    filter in progress

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const vacancies = document.querySelectorAll('.vacancy');
 const popupClose = document.querySelector('.popup__close');
 const popupWindow = document.querySelector('.popup__window');
+const vacanciesFilterArrow = document.querySelector('.vacancies__filter-arrow');
+const filterForm = document.querySelector('.filter__form');
+const html = document.querySelector('html');
 
 
 vacancies.forEach(vacancy => {
@@ -17,8 +20,28 @@ vacancies.forEach(vacancy => {
     })
 });
 
-
 popupClose.addEventListener('click', function() {
     popupWindow.classList.remove('show');
     popupWindow.classList.add('hide');
 })
+
+window.addEventListener('resize', function() {
+    let widthScreen = document.querySelector('body').offsetWidth;
+    let filterFormClasses = filterForm.classList;
+    if (widthScreen < 925) {
+        vacanciesFilterArrow.addEventListener('click', function() {
+            if (!filterFormClasses.contains('show__flex')) {
+                filterFormClasses.remove('hide');
+                filterFormClasses.add('show__flex');
+            } else {
+                filterFormClasses.remove('show__flex');
+                filterFormClasses.add('hide');
+            }
+        })
+    } else {
+        filterFormClasses.add('show__flex');
+    }
+});
+
+
+
