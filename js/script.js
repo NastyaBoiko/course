2546cc2c5c922cee8bf2f2003c10cb418441898f
Nastya <nastya@mail.com>
2023-11-21 17:44:46 +0300

    tab + open popup Enter

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
-const cardsItems = document.querySelectorAll('.cards__item');
-console.log(cardsItems);
+const vacancies = document.querySelectorAll('.vacancy');
 const popupClose = document.querySelector('.popup__close');
 const popupWindow = document.querySelector('.popup__window');
 
 
-cardsItems.forEach(cardsItem => {
-    cardsItem.addEventListener('click', function() {
+vacancies.forEach(vacancy => {
+    vacancy.addEventListener('click', function() {
         popupWindow.classList.remove('hide');
         popupWindow.classList.add('show');
+    })
+
+    vacancy.addEventListener('keypress', function(event) {
+        if (event.code === 'Enter') {
+            popupWindow.classList.remove('hide');
+            popupWindow.classList.add('show');
+        }
     })
 });
 
