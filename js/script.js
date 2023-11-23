c487bdea82b5271a8ca7be0d18f8f61332cd3550
Nastya <killer99whale@mail.ru>
2023-11-23 15:43:44 +0300

    js open popup and filter

--- js/script.js
+++ js/script.js
@@ -1,47 +1,31 @@
-const vacancies = document.querySelectorAll('.vacancy');
-const popupClose = document.querySelector('.popup__close');
-const popupWindow = document.querySelector('.popup__window');
-const vacanciesFilterArrow = document.querySelector('.vacancies__filter-arrow');
-const filterForm = document.querySelector('.filter__form');
-const html = document.querySelector('html');
+const vacancies = document.querySelectorAll(".vacancy");
+const popupClose = document.querySelector(".popup__close");
+const popupWindow = document.querySelector(".popup__window");
 
+const vacanciesFilterArrow = document.querySelector(".vacancies__filter-arrow");
+const filterForm = document.querySelector(".filter__form");
 
-vacancies.forEach(vacancy => {
-    vacancy.addEventListener('click', function() {
-        popupWindow.classList.remove('hide');
-        popupWindow.classList.add('show');
-    })
+// логика открытия popup
+vacancies.forEach((vacancy) => {
+  vacancy.addEventListener("click", function () {
+    popupWindow.classList.toggle("show");
+  });
 
-    vacancy.addEventListener('keypress', function(event) {
-        if (event.code === 'Enter') {
-            popupWindow.classList.remove('hide');
-            popupWindow.classList.add('show');
-        }
-    })
-});
-
-popupClose.addEventListener('click', function() {
-    popupWindow.classList.remove('show');
-    popupWindow.classList.add('hide');
-})
-
-window.addEventListener('resize', function() {
-    let widthScreen = document.querySelector('body').offsetWidth;
-    let filterFormClasses = filterForm.classList;
-    if (widthScreen < 925) {
-        vacanciesFilterArrow.addEventListener('click', function() {
-            if (!filterFormClasses.contains('show__flex')) {
-                filterFormClasses.remove('hide');
-                filterFormClasses.add('show__flex');
-            } else {
-                filterFormClasses.remove('show__flex');
-                filterFormClasses.add('hide');
-            }
-        })
-    } else {
-        filterFormClasses.add('show__flex');
+  vacancy.addEventListener("keypress", function (event) {
+    if (event.code === "Enter") {
+      popupWindow.classList.toggle("show");
     }
+  });
 });
 
+popupClose.addEventListener("click", function () {
+  popupWindow.classList.remove("show");
+});
 
-
+// Открытие, закрытие фильтров на экране < 925
+vacanciesFilterArrow.addEventListener("click", function () {
+  let widthScreen = document.querySelector("body").offsetWidth;
+  if (widthScreen < 925) {
+    filterForm.classList.toggle("show__flex");
+  }
+});
